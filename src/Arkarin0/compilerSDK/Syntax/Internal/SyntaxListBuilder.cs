e6d18bc9a0e790776a15ac06f60f091b33db5059
Arkarin0 <snaut@hotmail.de>
2022-01-22 11:19:11 +0100

    disabled class

--- src/Arkarin0/compilerSDK/Syntax/Internal/SyntaxListBuilder.cs
+++ src/Arkarin0/compilerSDK/Syntax/Internal/SyntaxListBuilder.cs
@@ -1,203 +1,203 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Diagnostics;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Arkarin0.CodeAnalysis.Syntax
-{
-    public class SyntaxListBuilder
-    {
-        private ArrayElement<GreenNode?>[] _nodes;
-        public int Count { get; private set; }
+//namespace Arkarin0.CodeAnalysis.Syntax
+//{
+//    public class SyntaxListBuilder
+//    {
+//        private ArrayElement<GreenNode?>[] _nodes;
+//        public int Count { get; private set; }
 
-        public SyntaxListBuilder(int size)
-        {
-            _nodes = new ArrayElement<GreenNode?>[size];
-        }
+//        public SyntaxListBuilder(int size)
+//        {
+//            _nodes = new ArrayElement<GreenNode?>[size];
+//        }
 
-        public static SyntaxListBuilder Create()
-        {
-            return new SyntaxListBuilder(8);
-        }
+//        public static SyntaxListBuilder Create()
+//        {
+//            return new SyntaxListBuilder(8);
+//        }
 
 
 
-        public void Clear()
-        {
-            this.Count = 0;
-        }
+//        public void Clear()
+//        {
+//            this.Count = 0;
+//        }
 
-        public GreenNode? this[int index]
-        {
-            get
-            {
-                return _nodes[index];
-            }
+//        public GreenNode? this[int index]
+//        {
+//            get
+//            {
+//                return _nodes[index];
+//            }
 
-            set
-            {
-                _nodes[index].Value = value;
-            }
-        }
+//            set
+//            {
+//                _nodes[index].Value = value;
+//            }
+//        }
 
-        public void Add(GreenNode? item)
-        {
-            if (item == null) return;
+//        public void Add(GreenNode? item)
+//        {
+//            if (item == null) return;
 
-            if (item.IsList)
-            {
-                int slotCount = item.SlotCount;
+//            if (item.IsList)
+//            {
+//                int slotCount = item.SlotCount;
 
-                // Necessary, but not sufficient (e.g. for nested lists).
-                EnsureAdditionalCapacity(slotCount);
+//                // Necessary, but not sufficient (e.g. for nested lists).
+//                EnsureAdditionalCapacity(slotCount);
 
-                for (int i = 0; i < slotCount; i++)
-                {
-                    this.Add(item.GetSlot(i));
-                }
-            }
-            else
-            {
-                EnsureAdditionalCapacity(1);
+//                for (int i = 0; i < slotCount; i++)
+//                {
+//                    this.Add(item.GetSlot(i));
+//                }
+//            }
+//            else
+//            {
+//                EnsureAdditionalCapacity(1);
 
-                _nodes[Count++].Value = item;
-            }
-        }
+//                _nodes[Count++].Value = item;
+//            }
+//        }
 
-        public void AddRange(GreenNode[] items)
-        {
-            this.AddRange(items, 0, items.Length);
-        }
+//        public void AddRange(GreenNode[] items)
+//        {
+//            this.AddRange(items, 0, items.Length);
+//        }
 
-        public void AddRange(GreenNode[] items, int offset, int length)
-        {
-            // Necessary, but not sufficient (e.g. for nested lists).
-            EnsureAdditionalCapacity(length - offset);
+//        public void AddRange(GreenNode[] items, int offset, int length)
+//        {
+//            // Necessary, but not sufficient (e.g. for nested lists).
+//            EnsureAdditionalCapacity(length - offset);
 
-            int oldCount = this.Count;
+//            int oldCount = this.Count;
 
-            for (int i = offset; i < length; i++)
-            {
-                Add(items[i]);
-            }
+//            for (int i = offset; i < length; i++)
+//            {
+//                Add(items[i]);
+//            }
 
-            Validate(oldCount, this.Count);
-        }
+//            Validate(oldCount, this.Count);
+//        }
 
-        [Conditional("DEBUG")]
-        private void Validate(int start, int end)
-        {
-            for (int i = start; i < end; i++)
-            {
-                Debug.Assert(_nodes[i].Value != null);
-            }
-        }
+//        [Conditional("DEBUG")]
+//        private void Validate(int start, int end)
+//        {
+//            for (int i = start; i < end; i++)
+//            {
+//                Debug.Assert(_nodes[i].Value != null);
+//            }
+//        }
 
-        public void AddRange(SyntaxList<GreenNode> list)
-        {
-            this.AddRange(list, 0, list.Count);
-        }
+//        public void AddRange(SyntaxList<GreenNode> list)
+//        {
+//            this.AddRange(list, 0, list.Count);
+//        }
 
-        public void AddRange(SyntaxList<GreenNode> list, int offset, int length)
-        {
-            // Necessary, but not sufficient (e.g. for nested lists).
-            EnsureAdditionalCapacity(length - offset);
+//        public void AddRange(SyntaxList<GreenNode> list, int offset, int length)
+//        {
+//            // Necessary, but not sufficient (e.g. for nested lists).
+//            EnsureAdditionalCapacity(length - offset);
 
-            int oldCount = this.Count;
+//            int oldCount = this.Count;
 
-            for (int i = offset; i < length; i++)
-            {
-                Add(list[i]);
-            }
+//            for (int i = offset; i < length; i++)
+//            {
+//                Add(list[i]);
+//            }
 
-            Validate(oldCount, this.Count);
-        }
+//            Validate(oldCount, this.Count);
+//        }
 
-        public void AddRange<TNode>(SyntaxList<TNode> list) where TNode : GreenNode
-        {
-            this.AddRange(list, 0, list.Count);
-        }
+//        public void AddRange<TNode>(SyntaxList<TNode> list) where TNode : GreenNode
+//        {
+//            this.AddRange(list, 0, list.Count);
+//        }
 
-        public void AddRange<TNode>(SyntaxList<TNode> list, int offset, int length) where TNode : GreenNode
-        {
-            this.AddRange(new SyntaxList<GreenNode>(list.Node), offset, length);
-        }
+//        public void AddRange<TNode>(SyntaxList<TNode> list, int offset, int length) where TNode : GreenNode
+//        {
+//            this.AddRange(new SyntaxList<GreenNode>(list.Node), offset, length);
+//        }
 
-        public void RemoveLast()
-        {
-            Count--;
-            _nodes[Count].Value = null;
-        }
+//        public void RemoveLast()
+//        {
+//            Count--;
+//            _nodes[Count].Value = null;
+//        }
 
-        private void EnsureAdditionalCapacity(int additionalCount)
-        {
-            int currentSize = _nodes.Length;
-            int requiredSize = this.Count + additionalCount;
+//        private void EnsureAdditionalCapacity(int additionalCount)
+//        {
+//            int currentSize = _nodes.Length;
+//            int requiredSize = this.Count + additionalCount;
 
-            if (requiredSize <= currentSize) return;
+//            if (requiredSize <= currentSize) return;
 
-            int newSize =
-                requiredSize < 8 ? 8 :
-                requiredSize >= (int.MaxValue / 2) ? int.MaxValue :
-                Math.Max(requiredSize, currentSize * 2); // NB: Size will *at least* double.
-            Debug.Assert(newSize >= requiredSize);
+//            int newSize =
+//                requiredSize < 8 ? 8 :
+//                requiredSize >= (int.MaxValue / 2) ? int.MaxValue :
+//                Math.Max(requiredSize, currentSize * 2); // NB: Size will *at least* double.
+//            Debug.Assert(newSize >= requiredSize);
 
-            Array.Resize(ref _nodes, newSize);
-        }
+//            Array.Resize(ref _nodes, newSize);
+//        }
 
-        public bool Any(int kind)
-        {
-            for (int i = 0; i < Count; i++)
-            {
-                if (_nodes[i].Value!.RawKind == kind)
-                {
-                    return true;
-                }
-            }
+//        public bool Any(int kind)
+//        {
+//            for (int i = 0; i < Count; i++)
+//            {
+//                if (_nodes[i].Value!.RawKind == kind)
+//                {
+//                    return true;
+//                }
+//            }
 
-            return false;
-        }
+//            return false;
+//        }
 
-        public GreenNode[] ToArray()
-        {
-            var array = new GreenNode[this.Count];
-            for (int i = 0; i < array.Length; i++)
-            {
-                array[i] = _nodes[i]!;
-            }
+//        public GreenNode[] ToArray()
+//        {
+//            var array = new GreenNode[this.Count];
+//            for (int i = 0; i < array.Length; i++)
+//            {
+//                array[i] = _nodes[i]!;
+//            }
 
-            return array;
-        }
+//            return array;
+//        }
 
-        internal GreenNode? ToListNode()
-        {
-            switch (this.Count)
-            {
-                case 0:
-                    return null;
-                case 1:
-                    return _nodes[0];
-                case 2:
-                    return SyntaxList.List(_nodes[0]!, _nodes[1]!);
-                case 3:
-                    return SyntaxList.List(_nodes[0]!, _nodes[1]!, _nodes[2]!);
-                default:
-                    var tmp = new ArrayElement<GreenNode>[this.Count];
-                    Array.Copy(_nodes, tmp, this.Count);
-                    return SyntaxList.List(tmp);
-            }
-        }
+//        internal GreenNode? ToListNode()
+//        {
+//            switch (this.Count)
+//            {
+//                case 0:
+//                    return null;
+//                case 1:
+//                    return _nodes[0];
+//                case 2:
+//                    return SyntaxList.List(_nodes[0]!, _nodes[1]!);
+//                case 3:
+//                    return SyntaxList.List(_nodes[0]!, _nodes[1]!, _nodes[2]!);
+//                default:
+//                    var tmp = new ArrayElement<GreenNode>[this.Count];
+//                    Array.Copy(_nodes, tmp, this.Count);
+//                    return SyntaxList.List(tmp);
+//            }
+//        }
 
-        //public SyntaxList<GreenNode> ToList()
-        //{
-        //    return new SyntaxList<GreenNode>(ToListNode());
-        //}
+//        //public SyntaxList<GreenNode> ToList()
+//        //{
+//        //    return new SyntaxList<GreenNode>(ToListNode());
+//        //}
 
-        //public SyntaxList<TNode> ToList<TNode>() where TNode : GreenNode
-        //{
-        //    return new SyntaxList<TNode>(ToListNode());
-        //}
-    }
-}
+//        //public SyntaxList<TNode> ToList<TNode>() where TNode : GreenNode
+//        //{
+//        //    return new SyntaxList<TNode>(ToListNode());
+//        //}
+//    }
+//}
