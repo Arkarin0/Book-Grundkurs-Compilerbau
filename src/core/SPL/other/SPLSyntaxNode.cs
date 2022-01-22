3d950a90f219e433a6925c7ec1d270f8e1179b2d
Arkarin0 <snaut@hotmail.de>
2022-01-22 16:55:25 +0100

    ab#16 implemented SPLSyntaxNode

--- src/core/SPL/other/SPLSyntaxNode.cs
+++ src/core/SPL/other/SPLSyntaxNode.cs
@@ -1,4 +1,5 @@
-﻿using Arkarin0.CodeAnalysis.Syntax;
+﻿using Arkarin0.CodeAnalysis;
+using Arkarin0.CodeAnalysis.Syntax;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,23 +10,62 @@ namespace BGC.CodeAnalysis.SPL.Syntax.InternalSyntax
 {
     internal abstract class SPLSyntaxNode:CustomSyntaxNode
     {
-        private readonly string _KindText;
-
-        public SPLSyntaxNode( SyntaxKind kind):base((ushort)kind)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SPLSyntaxNode"/> class.
+        /// </summary>
+        /// <param name="kind">The kind.</param>
+        public SPLSyntaxNode(SyntaxKind kind):base((ushort)kind)
         {
 
         }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SPLSyntaxNode"/> class.
+        /// </summary>
+        /// <param name="kind">The kind.</param>
+        /// <param name="fullWidth">The full width.</param>
+        protected SPLSyntaxNode(SyntaxKind kind, int fullWidth) : base((ushort)kind, fullWidth)
+        {
+        }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SPLSyntaxNode"/> class.
+        /// </summary>
+        /// <param name="kind">The kind.</param>
+        /// <param name="diagnostics">The diagnostics.</param>
+        protected SPLSyntaxNode(SyntaxKind kind, DiagnosticInfo[] diagnostics) : base((ushort)kind, diagnostics)
+        {
+        }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SPLSyntaxNode"/> class.
+        /// </summary>
+        /// <param name="kind">The kind.</param>
+        /// <param name="diagnostics">The diagnostics.</param>
+        /// <param name="fullWidth">The full width.</param>
+        protected SPLSyntaxNode(SyntaxKind kind, DiagnosticInfo[] diagnostics, int fullWidth) : base((ushort)kind, diagnostics, fullWidth)
+        {
+        }
 
-        public override string Language => "Simple Programming Language";
+        /// <inheritdoc/>
+        public sealed override string Language => "Simple Programming Language";
 
+        /// <summary>
+        /// Gets the kind of this SyntaxNode.
+        /// </summary>
         public SyntaxKind Kind
         {
             get { return (SyntaxKind)this.RawKind; }
         }
 
+        /// <inheritdoc/>
         public override string KindText => this.Kind.ToString();
+
+        /// <inheritdoc/>
+        public bool IsTriviaWithEndOfLine()
+        {
+            return this.Kind == SyntaxKind.EndOfLineTrivia
+                || this.Kind == SyntaxKind.SingleLineCommentTrivia;
+        }
 
     }
 }
---
+++ src/tests/spl/other/SPLSyntaxNodeTests.cs
@@ -0,0 +1,129 @@
+﻿using Xunit;
+using BGC.CodeAnalysis.SPL.Syntax.InternalSyntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Arkarin0.CodeAnalysis;
+
+namespace BGC.CodeAnalysis.SPL.Syntax.InternalSyntax.Tests
+{
+    public class SPLSyntaxNodeTests
+    {
+        internal class SPLSyntaxNode : Syntax.InternalSyntax.SPLSyntaxNode
+        {
+            public SPLSyntaxNode(SyntaxKind kind) : base(kind)
+            {
+            }
+
+            public SPLSyntaxNode(SyntaxKind kind, int fullWidth) : base(kind, fullWidth)
+            {
+            }
+
+            public SPLSyntaxNode(SyntaxKind kind, DiagnosticInfo[] diagnostics) : base(kind, diagnostics)
+            {
+            }
+
+            public SPLSyntaxNode(SyntaxKind kind, DiagnosticInfo[] diagnostics, int fullWidth) : base(kind, diagnostics, fullWidth)
+            {
+            }
+
+            public override GreenNode SetDiagnostics(DiagnosticInfo[] diagnostics)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private SPLSyntaxNode CreateValidInstance(SyntaxKind kind= SyntaxKind.None)
+        {
+            return new SPLSyntaxNode(kind);
+        }
+
+
+        [Fact()]
+        public void CtorTest()
+        {
+            SyntaxKind kind = SyntaxKind.OfKeyword;
+            var obj = new SPLSyntaxNode(kind);
+            Assert.Equal(kind, obj.Kind);
+
+            kind = SyntaxKind.SemicolonToken;
+            int length = 12;
+            obj = new SPLSyntaxNode(kind, length);
+            Assert.Equal(kind, obj.Kind);
+            Assert.Equal(length, obj.FullWidth);
+        }
+
+
+
+        [Fact()]
+        public void Ctor_WithDiagnosticInfoTest()
+        {
+            SyntaxKind kind = SyntaxKind.OfKeyword;
+            var obj = new SPLSyntaxNode(kind, null);
+            Assert.Equal(kind, obj.Kind);
+            Assert.NotEqual(GreenNode.NodeFlags.ContainsDiagnostics, obj.Flags);
+
+            kind = SyntaxKind.OpenBracketToken;
+            obj = new SPLSyntaxNode(kind, new DiagnosticInfo[] { new DiagnosticInfo(10) });
+            Assert.Equal(kind, obj.Kind);
+            Assert.Equal(GreenNode.NodeFlags.ContainsDiagnostics, obj.Flags);
+
+
+
+            kind = SyntaxKind.OpenParenToken;
+            int length = 12;
+            obj = new SPLSyntaxNode(kind, null, length);
+            Assert.Equal(kind, obj.Kind);
+            Assert.Equal(length, obj.FullWidth);
+            Assert.NotEqual(GreenNode.NodeFlags.ContainsDiagnostics, obj.Flags);
+
+            kind = SyntaxKind.PlusToken;
+            length = 13;
+            obj = new SPLSyntaxNode(kind, new DiagnosticInfo[] { new DiagnosticInfo(10) }, length);
+            Assert.Equal(kind, obj.Kind);
+            Assert.Equal(length, obj.FullWidth);
+            Assert.Equal(GreenNode.NodeFlags.ContainsDiagnostics, obj.Flags);
+        }
+
+        [Theory()]
+        [InlineData(SyntaxKind.EndOfLineTrivia, true)]
+        [InlineData(SyntaxKind.SingleLineCommentTrivia, true)]
+        [InlineData(SyntaxKind.IdentifierName, false)]
+        public void IsTriviaWithEndOfLineTest(SyntaxKind kind, bool expected)
+        {
+            var obj = new SPLSyntaxNode(kind);
+
+            var actual = obj.IsTriviaWithEndOfLine();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void LanguageNameTest()
+        {
+            var text = "Simple Programming Language";
+            var obj = CreateValidInstance();
+
+            Assert.Equal(text, obj.Language);
+
+        }
+
+        [Theory]
+        [InlineData(SyntaxKind.EndOfLineTrivia)]
+        [InlineData(SyntaxKind.AmpersandToken)]
+        [InlineData(SyntaxKind.OfKeyword)]
+        public void KindTest(SyntaxKind expected)
+        {            
+            var obj = CreateValidInstance(expected);
+
+            var actualKind = obj.Kind;
+            var actualKindText = obj.KindText;
+
+            Assert.Equal(expected, actualKind);
+            Assert.Equal(expected.ToString(), actualKindText);
+
+        }
+    }
+}
