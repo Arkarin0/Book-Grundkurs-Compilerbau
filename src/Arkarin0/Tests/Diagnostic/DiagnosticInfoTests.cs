96b46200ba2875ebb0e707d2cc33bae3e52810c4
Arkarin0 <snaut@hotmail.de>
2022-01-22 11:18:36 +0100

    ab#15 added DiagnosticInfo and Unittestproject

---
+++ src/Arkarin0/Tests/Diagnostic/DiagnosticInfoTests.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+using Arkarin0.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arkarin0.CodeAnalysis.Tests
+{
+    public class DiagnosticInfoTests
+    {
+        [Fact()]
+        public void DiagnosticInfoTest()
+        {
+            int errorcode = 10;
+
+            var obj = new DiagnosticInfo(errorcode);
+
+            Assert.Equal(errorcode, obj.Code);
+        }
+
+        [Fact()]
+        public void ToStringTest()
+        {
+            int errorcode = 10;
+            var obj = new DiagnosticInfo(errorcode);
+            var str = obj.ToString();
+
+            var expected = "error:" + errorcode;
+            Assert.Equal(expected, str);            
+        }
+    }
+}
---
+++ src/Arkarin0/compilerSDK/Diagnostic/DiagnosticInfo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arkarin0.CodeAnalysis
+{
+
+    /// <summary>
+    /// A DiagnosticInfo object has information about a diagnostic, but without any attached location information.
+    /// </summary>
+    /// <remarks>
+    /// More specialized diagnostics with additional information (e.g., ambiguity errors) can derive from this class to
+    /// provide access to additional information about the error, such as what symbols were involved in the ambiguity.
+    /// </remarks>
+    [DebuggerDisplay("{GetDebuggerDisplay(), nq}")]
+    public class DiagnosticInfo
+    {
+        private readonly int _errorCode;
+
+        /// <summary>
+        /// The error code, as an integer.
+        /// </summary>
+        public int Code { get { return _errorCode; } }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DiagnosticInfo"/> class.
+        /// </summary>
+        /// <param name="error">The error.</param>
+        public DiagnosticInfo(int error)
+        {
+            _errorCode = error;
+        }
+
+        public override string ToString()
+        {
+            return "error:" + this._errorCode;
+        }
+
+        private string GetDebuggerDisplay()
+        {
+            return this.GetType().Name + " " + this.ToString();
+        }
+    }
+}
