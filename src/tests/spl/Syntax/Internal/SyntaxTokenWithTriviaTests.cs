ed8e63227f83fcbd3a4ea154df9cd6f87f7b9879
Arkarin0 <snaut@hotmail.de>
2022-01-24 11:46:59 +0100

    ab#17 added SyntaxTokenWithTrivia

---
+++ src/tests/spl/Syntax/Internal/SyntaxTokenWithTriviaTests.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Arkarin0.CodeAnalysis;
+using static BGC.CodeAnalysis.SPL.Syntax.InternalSyntax.SyntaxToken;
+using BGC.SPL;
+
+namespace BGC.CodeAnalysis.SPL.Syntax.InternalSyntax.Tests
+{
+    public class SyntaxTokenWithTriviaTests
+    {
+        private SyntaxTokenWithTrivia CreateValidInstance(SyntaxKind kind = SyntaxKind.AsteriskToken)
+        {
+            return new SyntaxTokenWithTrivia(kind, null);
+        }
+
+        [Fact()]
+        public void CTorTest()
+        {
+            TestHelper.AssertCTorWithDiagnostics<SyntaxTokenWithTrivia>(hasWidth:false);
+        }
+
+        [Fact()]
+        public void SetDiagnosticsTest()
+        {
+            TestHelper.AssertSetDiagnostics(CreateValidInstance());
+        }
+    }
+}
