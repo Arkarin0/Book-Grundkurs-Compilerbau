35241e2f098d1dec68b8b4628a7aa3bc840a20bb
Arkarin0 <snaut@hotmail.de>
2022-01-24 11:09:39 +0100

    ab#17 Added SyntaxToken

--- src/core/SPL/Syntax/Internal/SyntaxToken.cs
+++ src/core/SPL/Syntax/Internal/SyntaxToken.cs
@@ -1,133 +1,152 @@
-﻿//using Arkarin0.CodeAnalysis;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using Arkarin0.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace BGC.CodeAnalysis.SPL.Syntax.InternalSyntax
-//{
-//    //using BGC.CodeAnalysis.Syntax.InternalSyntax;
-//    internal partial class SyntaxToken:SPLSyntaxNode
-//    {
- 
-//        public SyntaxToken(SyntaxKind kind)
-//            :base(kind)
-//        {
-            
-//        }
+namespace BGC.CodeAnalysis.SPL.Syntax.InternalSyntax
+{
+    //using BGC.CodeAnalysis.Syntax.InternalSyntax;
+    internal partial class SyntaxToken : SPLSyntaxNode
+    {
 
  
-//        public override GreenNode GetSlot(int index)
-//        {
-//            throw Sonea.Utilities.ExceptionUtilities.Unreachable;
-//        }
-
-//        internal static SyntaxToken Create(SyntaxKind kind)
-//        {
-//            if (kind > LastTokenWithWellKnownText)
-//            {
-//                if (!SyntaxFacts.IsAnyToken(kind))
-//                {
-//                    throw new ArgumentException($"This methode can only be used to create Tokens based on {typeof(SyntaxKind)}", nameof(kind));
-//                }
-
-//                return CreateMissing(kind, null, null);
-//            }
-
-//            return s_tokensWithNoTrivia[(int)kind].Value;
-//        }
-
-//        internal static SyntaxToken Create(SyntaxKind kind, GreenNode leading, GreenNode trailing)
-//        {
-//            if (kind > LastTokenWithWellKnownText)
-//            {
-//                if (!SyntaxFacts.IsAnyToken(kind))
-//                {
-//                    throw new ArgumentException($"This methode can only be used to create Tokens based on {typeof(SyntaxKind)}", nameof(kind));
-//                }
-
-//                return CreateMissing(kind, leading, trailing);
-//            }
-
-//            if (leading == null)
-//            {
-//                if (trailing == null)
-//                {
-//                    return s_tokensWithNoTrivia[(int)kind].Value;
-//                }
-//                else if (trailing == SyntaxFactory.Space)
-//                {
-//                    return s_tokensWithSingleTrailingSpace[(int)kind].Value;
-//                }
-//                else if (trailing == SyntaxFactory.CarriageReturnLineFeed)
-//                {
-//                    return s_tokensWithSingleTrailingCRLF[(int)kind].Value;
-//                }
-//            }
-                       
-
-//            return new SyntaxTokenWithTrivia(kind, leading, trailing);
-//        }
-
-//        internal static SyntaxToken CreateMissing(SyntaxKind kind, GreenNode leading, GreenNode trailing)
-//        {
-//            return new MissingTokenWithTrivia(kind, leading, trailing);
-//        }
-
-//        internal const SyntaxKind FirstTokenWithWellKnownText = SyntaxKind.TildeToken;
-//        internal const SyntaxKind LastTokenWithWellKnownText = SyntaxKind.EndOfFileToken;
-
-//        private static readonly ArrayElement<SyntaxToken>[] s_tokensWithNoTrivia = new ArrayElement<SyntaxToken>[(int)LastTokenWithWellKnownText + 1];
-//        private static readonly ArrayElement<SyntaxToken>[] s_tokensWithSingleTrailingSpace = new ArrayElement<SyntaxToken>[(int)LastTokenWithWellKnownText + 1];
-//        private static readonly ArrayElement<SyntaxToken>[] s_tokensWithSingleTrailingCRLF = new ArrayElement<SyntaxToken>[(int)LastTokenWithWellKnownText + 1];
-
-//        static SyntaxToken()
-//        {
-//            //ObjectBinder.RegisterTypeReader(typeof(SyntaxToken), r => new SyntaxToken(r));
-
-//            for (var kind = FirstTokenWithWellKnownText; kind <= LastTokenWithWellKnownText; kind++)
-//            {
-//                s_tokensWithNoTrivia[(int)kind].Value = new SyntaxToken(kind);
-                
-//                s_tokensWithSingleTrailingSpace[(int)kind].Value = new SyntaxTokenWithTrivia(kind, null, SyntaxFactory.Space);
-//                s_tokensWithSingleTrailingCRLF[(int)kind].Value = new SyntaxTokenWithTrivia(kind, null, SyntaxFactory.CarriageReturnLineFeed);
-//            }
-//        }
-
-//        /// <inheritdoc cref="ToString"/>        
-//        public virtual string Text
-//        {
-//            get { return SyntaxFacts.GetText(this.Kind); }
-//        }
-
-//        /// <summary>
-//        /// Returns the string representation of this token, not including its leading and trailing trivia.
-//        /// </summary>
-//        /// <returns>The string representation of this token, not including its leading and trailing trivia.</returns>
-//        /// <remarks>The length of the returned string is always the same as Span.Length</remarks>
-//        public override string ToString()
-//        {
-//            return this.Text;
-//        }
 
 
-//        public virtual object Value
-//        {
-//            get
-//            {
-//                switch (this.Kind)
-//                {
-//                    //case SyntaxKind.TrueKeyword:
-//                    //    return Boxes.BoxedTrue;
-//                    //case SyntaxKind.FalseKeyword:
-//                    //    return Boxes.BoxedFalse;
-//                    //case SyntaxKind.NullKeyword:
-//                    //    return null;
-//                    default:
-//                        return this.Text;
-//                }
-//            }
-//        }
-//    }
-//}
+        //public override GreenNode GetSlot(int index)
+        //{
+        //    throw Sonea.Utilities.ExceptionUtilities.Unreachable;
+        //}
+
+        internal static SyntaxToken Create(SyntaxKind kind)
+        {
+            if (kind > LastTokenWithWellKnownText)
+            {
+                if (!SyntaxFacts.IsAnyToken(kind))
+                {
+                    throw new ArgumentException($"This methode can only be used to create Tokens based on {typeof(SyntaxKind)}", nameof(kind));
+                }
+
+                //return CreateMissing(kind, null, null);
+            }
+
+            return s_tokensWithNoTrivia[(int)kind].Value;
+        }
+
+        //internal static SyntaxToken Create(SyntaxKind kind, GreenNode leading, GreenNode trailing)
+        //{
+        //    if (kind > LastTokenWithWellKnownText)
+        //    {
+        //        if (!SyntaxFacts.IsAnyToken(kind))
+        //        {
+        //            throw new ArgumentException($"This methode can only be used to create Tokens based on {typeof(SyntaxKind)}", nameof(kind));
+        //        }
+
+        //        return CreateMissing(kind, leading, trailing);
+        //    }
+
+        //    if (leading == null)
+        //    {
+        //        if (trailing == null)
+        //        {
+        //            return s_tokensWithNoTrivia[(int)kind].Value;
+        //        }
+        //        else if (trailing == SyntaxFactory.Space)
+        //        {
+        //            return s_tokensWithSingleTrailingSpace[(int)kind].Value;
+        //        }
+        //        else if (trailing == SyntaxFactory.CarriageReturnLineFeed)
+        //        {
+        //            return s_tokensWithSingleTrailingCRLF[(int)kind].Value;
+        //        }
+        //    }
+
+
+        //    return new SyntaxTokenWithTrivia(kind, leading, trailing);
+        //}
+
+        //internal static SyntaxToken CreateMissing(SyntaxKind kind, GreenNode leading, GreenNode trailing)
+        //{
+        //    return new MissingTokenWithTrivia(kind, leading, trailing);
+        //}
+
+        internal const SyntaxKind FirstTokenWithWellKnownText = SyntaxKind.TildeToken;
+        internal const SyntaxKind LastTokenWithWellKnownText = SyntaxKind.EndOfFileToken;
+
+        private static readonly ArrayElement<SyntaxToken>[] s_tokensWithNoTrivia = new ArrayElement<SyntaxToken>[(int)LastTokenWithWellKnownText + 1];
+        private static readonly ArrayElement<SyntaxToken>[] s_tokensWithSingleTrailingSpace = new ArrayElement<SyntaxToken>[(int)LastTokenWithWellKnownText + 1];
+        private static readonly ArrayElement<SyntaxToken>[] s_tokensWithSingleTrailingCRLF = new ArrayElement<SyntaxToken>[(int)LastTokenWithWellKnownText + 1];
+
+        static SyntaxToken()
+        {
+            //ObjectBinder.RegisterTypeReader(typeof(SyntaxToken), r => new SyntaxToken(r));
+
+            //prebuild wellknown Tokens
+            for (var kind = FirstTokenWithWellKnownText; kind <= LastTokenWithWellKnownText; kind++)
+            {
+                s_tokensWithNoTrivia[(int)kind].Value = new SyntaxToken(kind);
+
+                //s_tokensWithSingleTrailingSpace[(int)kind].Value = new SyntaxTokenWithTrivia(kind, null, SyntaxFactory.Space);
+                //s_tokensWithSingleTrailingCRLF[(int)kind].Value = new SyntaxTokenWithTrivia(kind, null, SyntaxFactory.CarriageReturnLineFeed);
+            }
+        }
+
+        public SyntaxToken(SyntaxKind kind): base(kind)
+        {
+
+        }
+
+        public SyntaxToken(SyntaxKind kind, int fullWidth) : base(kind, fullWidth)
+        {
+        }
+
+        public SyntaxToken(SyntaxKind kind, DiagnosticInfo[] diagnostics) : base(kind, diagnostics)
+        {
+        }
+
+        public SyntaxToken(SyntaxKind kind, DiagnosticInfo[] diagnostics, int fullWidth) : base(kind, diagnostics, fullWidth)
+        {
+        }
+
+        /// <inheritdoc cref="ToString"/>        
+        public virtual string Text
+        {
+            get { return SyntaxFacts.GetText(this.Kind); }
+        }
+
+        /// <summary>
+        /// Returns the string representation of this token, not including its leading and trailing trivia.
+        /// </summary>
+        /// <returns>The string representation of this token, not including its leading and trailing trivia.</returns>
+        /// <remarks>The length of the returned string is always the same as Span.Length</remarks>
+        public override string ToString()
+        {
+            return this.Text;
+        }
+
+        /// <inheritdoc/>
+        public override GreenNode SetDiagnostics(DiagnosticInfo[] diagnostics)
+        {
+            return new SyntaxToken(this.Kind, diagnostics);
+        }
+
+        public virtual object Value
+        {
+            get
+            {
+                switch (this.Kind)
+                {
+                    //case SyntaxKind.TrueKeyword:
+                    //    return Boxes.BoxedTrue;
+                    //case SyntaxKind.FalseKeyword:
+                    //    return Boxes.BoxedFalse;
+                    //case SyntaxKind.NullKeyword:
+                    //    return null;
+                    default:
+                        return this.Text;
+                }
+            }
+        }
+    }
+}
---
+++ src/tests/spl/Syntax/Internal/SyntaxTokenTests.cs
@@ -0,0 +1,79 @@
+﻿using Xunit;
+using BGC.CodeAnalysis.SPL.Syntax.InternalSyntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Arkarin0.CodeAnalysis;
+using BGC.SPL;
+
+namespace BGC.CodeAnalysis.SPL.Syntax.InternalSyntax.Tests
+{
+    public class SyntaxTokenTests
+    {
+        private SyntaxToken CreateValidInstance(SyntaxKind kind= SyntaxKind.AsteriskToken)
+        {
+            return new SyntaxToken(kind);
+        }
+
+        [Fact()]
+        public void CtorTest()
+        {
+            TestHelper.AssertCTor<SyntaxToken>();
+        }
+
+
+
+        [Fact()]
+        public void Ctor_WithDiagnosticInfoTest()
+        {            
+            TestHelper.AssertCTorWithDiagnostics<SyntaxToken>();
+        }
+
+        [Fact()]
+        public void ToStringTest()
+        {
+            var expected = "*";
+            var obj = CreateValidInstance(SyntaxKind.AsteriskToken);
+
+            Assert.Equal(expected, obj.ToString());
+        }
+
+        [Fact()]
+        public void ValueTest()
+        {
+            var expected = "*";
+            var obj = CreateValidInstance(SyntaxKind.AsteriskToken);
+
+            Assert.Equal(expected, obj.Value);
+        }
+
+        [Fact()]
+        public void TextTest()
+        {
+            var expected = "*";
+            var obj = CreateValidInstance(SyntaxKind.AsteriskToken);
+
+            Assert.Equal(expected, obj.Text);
+        }
+
+        [Fact()]
+        public void SetDiagnosticsTest()
+        {
+            TestHelper.AssertSetDiagnostics(CreateValidInstance());
+        }
+
+        [Fact()]
+        public void Create_TokenIsKnownTest()
+        {
+            var expectedKind= SyntaxKind.AsteriskToken;
+
+            var obj= SyntaxToken.Create(expectedKind);
+
+            Assert.IsType<SyntaxToken>(obj);
+            Assert.Equal(expectedKind, obj.Kind);
+        }
+
+    }
+}
